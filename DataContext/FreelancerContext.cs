053370c6ee76274b2526bf413599291d264b8ae3
avigailrapa <avigailrapa@gmail.com>
2026-02-03 20:04:18 +0200

    feat(Data): setup data layer and db context

---
+++ DataContext/FreelancerContext.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+using Repository.interfaces;
+
+
+namespace FreelancersApi.DataContext
+{
+	public class FreelancerContext : DbContext, IContext
+	{
+		private readonly string connection;
+		public FreelancerContext(string connection)
+		{
+			this.connection = connection;
+		}
+		public virtual DbSet<User> Users { get; set; }
+		public virtual DbSet<Freelancer> Freelancers { get; set; }
+		public virtual DbSet<Category> Categories { get; set; }
+		public virtual DbSet<Job> Jobs { get; set; }
+		public virtual DbSet<Rating> Ratings { get; set; }
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(connection);
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// קשר בין Rating ל-User
+			modelBuilder.Entity<Rating>()
+				.HasOne(r => r.User)
+				.WithMany(u => u.RatingsGiven)
+				.HasForeignKey(r => r.UserId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			// קשר בין Rating ל-Freelancer
+			modelBuilder.Entity<Rating>()
+				.HasOne(r => r.Freelancer)
+				.WithMany(f => f.RatingsReceived)
+				.HasForeignKey(r => r.FreelancerId)
+				.OnDelete(DeleteBehavior.Cascade);
+		}
+
+		public async Task Save()
+		{
+			await SaveChangesAsync();
+		}
+
+
+	}
+}
---
+++ Repository/interfaces/IContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Repository.Entities;
+
+
+namespace Repository.interfaces
+{
+	public interface IContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Freelancer> Freelancers { get; set; }
+		public DbSet<Job> Jobs { get; set; }
+		public DbSet<Rating> Ratings { get; set; }
+		public Task Save();
+
+	}
+}
