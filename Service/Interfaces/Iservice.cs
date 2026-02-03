5395bf2d7aa45204fa32fb75d0db8d9dfe89288d
avigailrapa <avigailrapa@gmail.com>
2026-02-03 21:14:43 +0200

    feat(Service): setup service layer and interfaces

---
+++ Service/Interfaces/Iservice.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.Interfaces
+{
+	internal interface IService<T>
+	{
+		Task<List<T>> GetAll();
+		Task<T> GetById(int id);
+		Task<T> AddItem(T item);
+		Task<T> UpdateItem(int id, T item);
+		Task DeleteItem(int id);
+
+	}
+}
