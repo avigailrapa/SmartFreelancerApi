6a1989f70d7a2e8765014f3b80fb1b68f4611541
avigailrapa <avigailrapa@gmail.com>
2026-02-05 21:32:30 +0200

    feat(Service): add AutoMapper profiles

---
+++ Service/Services/MapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Common.Dto;
+using Repository.Entities;
+
+namespace Service.Services
+{
+    internal class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+			CreateMap<User, UserDto>().ReverseMap();
+			CreateMap<Freelancer, FreelancerDto>().ReverseMap();
+			CreateMap<Job, JobDto>().ReverseMap();
+			CreateMap<Rating, RatingDto>().ReverseMap();
+			CreateMap<Category, CategoryDto>().ReverseMap();
+
+
+		}
+	}
+}
