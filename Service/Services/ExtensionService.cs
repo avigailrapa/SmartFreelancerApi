27f05e731eb73b294e5a4b1d233108054d91c97a
avigailrapa <avigailrapa@gmail.com>
2026-02-06 12:38:59 +0200

    feat(DI): register all services in ExtensionService

---
+++ Service/Services/ExtensionService.cs
@@ -0,0 +1,24 @@
+﻿using Common.Dto;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.Repositories;
+using Service.Interfaces;
+
+namespace Service.Services
+{
+    public static class ExtensionService
+    {
+		public static IServiceCollection AddServices(this IServiceCollection services)
+		{
+			services.AddRepository();
+			services.AddScoped<IService<CategoryDto>, CategoryService>();
+			//services.AddScoped<IsExist<BabyDto>, BabyService>();
+			services.AddScoped<IService<UserDto>, UserService>();
+			//services.AddScoped<IsExist<NurseDto>, NurseService>();
+			services.AddScoped<IService<JobDto>, JobService>();
+			services.AddScoped<IService<FreelancerDto>, FreelancerService>();
+			services.AddScoped<IService<RatingDto>, RatingService>();
+
+			return services;
+		}
+	}
+}
